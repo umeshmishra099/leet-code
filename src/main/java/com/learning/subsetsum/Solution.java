9dab5cf5f3bf5f5ca81efc1a8e35e0720b73a08c
umesh <umesmishra099@gmail.com>
2020-06-24 19:33:53 +0530

    input update

--- src/main/java/com/learning/subsetsum/Solution.java
+++ src/main/java/com/learning/subsetsum/Solution.java
@@ -4,8 +4,12 @@ public class Solution
 {
   public static void main(String[] args)
   {
-    int [] input = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,100};
+    int[] input = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 100
+    };
     System.out.println(subSetSum(input, input.length, 99));
+    System.out.println(subsetSum(input, 100));
   }
 
   private static boolean subSetSum(int[] nums, int n, int sum)
@@ -41,6 +45,33 @@ public class Solution
       }
     }
     return result[n][sum];
+  }
+
+  public static int subsetSum(int[] A, int sum)
+  {
+    int n = A.length;
+    int[][] T = new int[n + 1][sum + 1];
+
+    for (int i = 0; i <= n; i++)
+    {
+      T[i][0] = 1;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+      for (int j = 1; j <= sum; j++)
+      {
+        if (A[i - 1] > j)
+        {
+          T[i][j] = T[i - 1][j];
+        }
+        else
+        {
+          T[i][j] = T[i - 1][j] + T[i - 1][j - A[i - 1]];
+        }
+      }
+    }
+    return T[n][sum];
   }
 
 }
