6c7237709c84e57a8f44a75293a87d283c8c847f
umesh <umesmishra099@gmail.com>
2020-07-02 18:30:31 +0530

    Updated algorithm to find top k element

--- src/main/java/com/topkthfreq/TopKFrequentElement.java
+++ src/main/java/com/topkthfreq/TopKFrequentElement.java
@@ -4,17 +4,17 @@ import java.util.*;
 
 /**
  * Given a non-empty array of integers, return the k most frequent elements.
- *
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: nums = [1,1,1,2,2,3], k = 2
  * Output: [1,2]
  * Example 2:
- *
+ * <p>
  * Input: nums = [1], k = 1
  * Output: [1]
  * Note:
- *
+ * <p>
  * You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
  * Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
  * It's guaranteed that the answer is unique, in other words the set of the top k frequent elements is unique.
@@ -24,8 +24,8 @@ public class TopKFrequentElement
 {
   public static void main(String[] args)
   {
-    int[] input = {1, 1, 1, 2, 2, 3,2,2,3,1,5,6,5,3,6,7,6,6,6,6,8};
-    Arrays.stream(topKFrequent(input, 1)).forEach(System.out::println);
+    int[] input = {1, 1, 1, 2, 2, 3, 2, 2, 3, 1, 5, 6, 5, 3, 6, 7, 6, 6, 6, 6, 8};
+    Arrays.stream(topKFrequent(input, 2)).forEach(System.out::println);
   }
 
   private static int[] topKFrequent(int[] nums, int k)
@@ -44,9 +44,24 @@ public class TopKFrequentElement
     }
 
     // sort uniques based on its frequency.
-    int inputLength = uniques.length;
-    sort(uniques, countFreq);
-    return Arrays.copyOfRange(uniques, inputLength - k, inputLength);
+    PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(
+            (a, b) -> a.getValue().equals(b.getValue()) ? b.getKey().compareTo(a.getKey()) : a.getValue() - b.getValue()
+    );
+
+    for (Map.Entry<Integer, Integer> entry : countFreq.entrySet())
+    {
+      pq.offer(entry);
+      if (pq.size() > k)
+        pq.poll();
+    }
+    int[] result = new int[k];
+    index = 0;
+    while (!pq.isEmpty())
+    {
+      result[index] = pq.poll().getKey();
+      index = index + 1;
+    }
+   return result;
   }
 
   private static void sort(int[] input, Map<Integer, Integer> count)
